76a64ace7296219c7d3683db82d059e83dae16b0
DavidRileySchultz <rileyschultz@gmail.com>
2018-09-13 11:30:22 -0500

    Added in methods to prompt for contestant information

--- Sweepstakes/UserInterface.cs
+++ Sweepstakes/UserInterface.cs
@@ -8,5 +8,26 @@ namespace Sweepstakes
 {
     class UserInterface
     {
+        public void PromptForContestantsFirstName()
+        {
+            Console.WriteLine("Please enter the contestant's First Name:");
+            Console.ReadLine();
+        }
+        public void PromptForContestantsLastName()
+        {
+            Console.WriteLine("Please enter the contestant's Last Name:");
+            Console.ReadLine();
+        }
+        public void PromptForContestsEmail()
+        {
+            Console.WriteLine("Please ebter the contestant's E-Mail address:");
+            Console.ReadLine();
+        }
+        public void PromptForContestantsId()
+        {
+            Console.WriteLine("Please enter contestant's ID number:");
+            Console.ReadLine();
+        }
+
     }
 }
