d8732411e01b6c35716f7e65aae3a067d47d6e99
DavidRileySchultz <rileyschultz@gmail.com>
2018-09-13 13:48:12 -0500

    Getting close to the completion of queue and stack structures.

--- Sweepstakes/SweepstakesQueueManager.cs
+++ Sweepstakes/SweepstakesQueueManager.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Sweepstakes
 {
-    class SweepstakesQueueManager
+    class SweepstakesQueueManager : ISweepstakesManager
     {
+        Queue<Contestant> sweepstakesQueue = new Queue<Contestant>();
+        public int Count
+        {
+            get
+            {
+                return sweepstakesQueue.Count;
+            }
+        }
+        public void AddIntoSweepSstakes(Contestant contestant)
+        {
+            sweepstakesQueue.Enqueue(contestant);
+        }
+        public void EndOfSweepstakes()
+        {
+            sweepstakesQueue.Dequeue();//use random generator to scramble to pick last person of list to be winner?
+        }
+        public Sweepstakes GetSweepstakes()
+        {
+            return sweepstakesQueue.First();
+        }
     }
 }
--- Sweepstakes/SweepstakesStackManager.cs
+++ Sweepstakes/SweepstakesStackManager.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Sweepstakes
 {
-    class SweepstakesStackManager
+    class SweepstakesStackManager : ISweepstakesManager
     {
+        Stack<Contestant> sweepstakesStack = new Stack<Contestant>();
+        public int Count
+        {
+            get
+            {
+                return sweepstakesStack.Count;
+            }
+        }
+        public void AddIntoSweepstakes(Contestant contestant)
+        {
+            sweepstakesStack.Push(contestant);
+        }
+        public void EndOfSweepstakes()
+        {
+            sweepstakesStack.Pop();//use random generator to pick last person of list to be winner?
+        }
+        public Sweepstakes GetSweepStakes()
+        {
+            return sweepstakesStack.First();
+        }
     }
 }
